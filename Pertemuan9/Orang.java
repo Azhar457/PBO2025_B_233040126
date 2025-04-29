17ae107787d0e8d16fd6ea67577e048b1ffa8b87
Azhar457 <azharsss457@gmail.com>
2025-04-29 09:38:09 +0700

    Upload done Latihan 4

---
+++ Pertemuan9/Orang.java
@@ -0,0 +1,24 @@
+package Pertemuan9;
+
+public class Orang{
+    protected String nama;
+    protected String alamat;
+
+    public Orang(String nama, String alamat){
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public String getNama(){
+        return nama;
+    }
+    public void setAlamat(String alamat){
+        this.alamat = alamat;
+    }
+    public String getAlamat(){
+        return alamat;
+    }
+}
